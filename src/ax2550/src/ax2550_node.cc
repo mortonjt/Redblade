829865e4f0a58b8502b7a55a18ee121c9f971df6
redblade <redblade@redblade-miami.(none)>
2013-10-11 13:44:17 -0400

    test

--- src/ax2550/src/ax2550_node.cc
+++ src/ax2550/src/ax2550_node.cc
@@ -246,7 +246,7 @@ int main(int argc, char **argv) {
     
     // Encoder Publisher
     encoder_pub = n.advertise<ax2550::StampedEncoders>("encoders", 5);
-    
+    //i suck at git
     // cmd_vel Subscriber
     ros::Subscriber sub = n.subscribe("cmd_vel", 1, cmd_velCallback);
     
