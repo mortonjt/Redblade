ebd87caf03674c66e1c59eeacc5bca7c54bee5cd
Bob Cole <colerc2@miamioh.edu>
2013-11-04 12:28:53 -0500

    more pid updates

--- src/snowplow_pid/src/snowplow_pid_node.cpp
+++ src/snowplow_pid/src/snowplow_pid_node.cpp
@@ -2,6 +2,7 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Pose2D.h>
 #include <string>
 #include <cmath>
 
@@ -10,8 +11,16 @@ ros::Publisher cmd_vel_pub;
 geometry_msgs::Twist vel_targets;
 geometry_msgs::Vector3 current_imu;
 nav_msgs::Odometry current_gps;
-
+geometry_msgs::Pose2D start;
+geometry_msgs::Pose2D dest;
+bool forward; //whether or not we go forwards or backwards to this waypoint
 bool imu_init = false;
+//global stuff for pid controller
+double previous_error;
+double sum_of_errors;
+double total_num_of_errors;
+double error;
+
 
 //keeps angles between -pi and pi so i don't have to
 void wrap_pi(double &angle){
@@ -22,6 +31,33 @@ void wrap_pi(double &angle){
       angle += 2*M_PI;
     }
   }
+}
+
+bool ye_ol_pid(){
+  //local variables
+  double desired_heading;
+  double current_heading = current_imu.z;
+
+  //TODO: implement a check for stuck method here
+  
+  //if this is the first time this method has been called, let's just send her in a straight line
+  //for a very short peiod of time
+  if(vel_targets.linear.x == 0){
+    vel_targets.linear.x = (forward?(0.75):(-0.75));// m/s
+    vel_targets.angular.z = 0;//straight line, no turnin
+    return false;//we ain't done yet
+  }
+  
+  //calculate error
+  desired_heading = atan2(dest.y-current_gps.pose.pose.position.y,
+			  dest.x-current_gps.pose.pose.position.x);
+  wrap_pi(desired_heading);
+  if(!forward){
+    current_heading -= M_PI;
+    wrap_pi(current_heading);
+  }
+
+  return false;
 }
 
 //Callback for the imu
@@ -37,7 +73,16 @@ void gpsCallback(const nav_msgs::Odometry::ConstPtr& gps_msg){
     //copy this info
     current_gps = *gps_msg;
 
-    //
+    //do the ol pid dance
+    if(ye_ol_pid()){
+      //TODO: use service to grab the next waypoint
+
+      //reinitialize all errors to zero
+      previous_error = 0;
+      sum_of_errors = 0;
+      total_num_of_errors = 0;
+      error = 0;
+    }
   }
 }
 
@@ -76,6 +121,12 @@ int main(int argc, char** argv){
   vel_targets.angular.y = 0;
   vel_targets.angular.z = 0;
 
+  //initialize pid errors to zero
+  previous_error = 0;
+  sum_of_errors = 0;
+  total_num_of_errors = 0;
+  error = 0;
+ 
   while(ros::ok()){
     publish_loop();
     
