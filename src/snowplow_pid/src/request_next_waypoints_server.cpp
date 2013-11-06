3619eed96e2f6f10fc70dd8585dc072babdbc160
Bob Cole <colerc2@miamioh.edu>
2013-11-06 17:56:30 -0500

    parameter for waypoint service

--- src/snowplow_pid/src/request_next_waypoints_server.cpp
+++ src/snowplow_pid/src/request_next_waypoints_server.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "snowplow_pid/request_next_waypoints.h"
 
+std::string waypoints_filename;
+
 bool next_waypoints(snowplow_pid::request_next_waypoints::Request &req,
 		    snowplow_pid::request_next_waypoints::Response &res){
   res.start.x = 10;
@@ -15,9 +17,11 @@ bool next_waypoints(snowplow_pid::request_next_waypoints::Request &req,
 int main(int argc, char** argv){
   ros::init (argc, argv, "request_next_waypoints_server");
   ros::NodeHandle n;
+  ros::NodeHandle nh("~");
 
   //TODO: add waypoints vector and populate this vector with waypoints
   //read in from some file that was passed in as a parameter
+  nh.param("waypoints_filename", waypoints_filename, std::string("waypoints.txt"));
 
   ros::ServiceServer service = n.advertiseService("request_next_waypoints", next_waypoints);
   ROS_INFO("Ready to provide next waypoints.");
