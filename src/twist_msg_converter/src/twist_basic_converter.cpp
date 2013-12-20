100add6b943861901f6d375d90b1ab0417d3c552
Jamie <jamietmorton@gmail.com>
2013-12-20 13:26:21 -0500

    New way of handling twist_msgs

--- src/twist_msg_converter/src/twist_basic_converter.cpp
+++ src/twist_msg_converter/src/twist_basic_converter.cpp
@@ -1,37 +1,35 @@
-#include "twist_converter.h"
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Vector3.h"
+#include <string>
+#include <cmath>
 
 static double clicks_per_m = 15768.6;
 static double wheel_base_width;
-static double wheel_base_length; //Distance between front and back set of wheels 
-
+static double wheel_base_length;
 
 ros::Publisher robo_front_pub;
 ros::Publisher robo_back_pub;
-geometry_msgs::Twist cmd;
-
 geometry_msgs::Twist front_target;
 geometry_msgs::Twist back_target;
 
-twist_converter::twist_converter(double yL,double yR){
-  this->yL=yL;
-  this->yR=yR;
-}
-
-twist_converter::~twist_converter(){
-  
-}
-
-void twist_converter::getVelocities(geometry_msgs::Twist& robot_twist,
-				    geometry_msgs::Twist& front_twist,
-				    geometry_msgs::Twist& back_twist){
-  double vx = robot_twist.linear.x;
-  double w  = robot_twist.angular.z;
-  double Vl = vx+yL*w;
-  double Vr = vx+yR*w;
-}
-
 void cmd_velCallback(const geometry_msgs::Twist::ConstPtr& msg){
-  cmd = *msg;
+  //for now, we're just going to push the overall linear and angular velocity through
+  //to each of the motor controllers, we'll figure out what's actually happening later
+  front_target.linear.x = msg->linear.x;
+  front_target.linear.y = msg->linear.y;
+  front_target.linear.z = msg->linear.z;
+  front_target.angular.x = msg->angular.x;
+  front_target.angular.y = msg->angular.y;
+  front_target.angular.z = msg->angular.z/2;
+
+  back_target.linear.x = msg->linear.x;
+  back_target.linear.y = msg->linear.y;
+  back_target.linear.z = msg->linear.z;
+  back_target.angular.x = msg->angular.x;
+  back_target.angular.y = msg->angular.y;
+  back_target.angular.z = msg->angular.z/2;
+
 }
 
 void Arduino_RC_Callback(const geometry_msgs::Twist::ConstPtr& msg) {
@@ -55,8 +53,7 @@ void Arduino_RC_Callback(const geometry_msgs::Twist::ConstPtr& msg) {
 
 
 /*This method is called once every 25 ms and will publish a Twist message for both the front and back roboteqs.*/
-void publish_loop(twist_converter& twistConv){
-  twistConv.getVelocities(cmd,front_target,back_target);
+void publish_loop(){
   //publish target Twist messages
   robo_front_pub.publish(front_target);
   robo_back_pub.publish(back_target);
@@ -66,15 +63,16 @@ int main(int argc, char** argv){
   //Node setup
   ros::init(argc, argv, "twist_conv_node");
   ros::NodeHandle n; //in the global namespace
-  ros::NodeHandle nh("~");//local namespace, used for params
-  double yICR_L,yICR_R; //Instantaneous centers of rotation
-  n.param("left_instantaneous_center_of_rotation", yICR_L, 1.0);
-  n.param("right_instantaneous_center_of_rotation", yICR_R, 1.0);
-
-  twist_converter twistConv(yICR_L,yICR_R);
 
   //Subscribe to cmd_vel topic
   ros::Subscriber cmd_vel_sub = n.subscribe("cmd_vel", 1, cmd_velCallback);
+
+  //Arduino RC subscriber
+  ros::Subscriber sub_arduino = n.subscribe("Arduino_RC", 5, Arduino_RC_Callback);
+
+  //Start Spinner so that cmd_vel Callbacks happen in a seperate thread
+  ros::AsyncSpinner spinner(2);
+  spinner.start();
 
   //set up two publishers, one to publish a Twist message for each roboteq
   robo_front_pub = n.advertise<geometry_msgs::Twist>("roboteq_front/cmd_vel", 10);
@@ -100,14 +98,16 @@ int main(int argc, char** argv){
   back_target.angular.x = 0;
   back_target.angular.y = 0;
   back_target.angular.z = 0;
-    
+  
+  
   //publish cmd_vel topic every 25 ms (40 hz)
   while(ros::ok()){
-    publish_loop(twistConv);
-    ros::spinOnce();
+    publish_loop();
+
     //sleep for a bit to stay at 40 hz
     cmd_vel_rate.sleep();
-    
   }
 
+  spinner.stop();
+  
 }
