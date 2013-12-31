23960407c816b4d5e5e644815926f96c370134b2
Bob Cole <colerc2@miamioh.edu>
2013-12-30 20:04:52 -0500

    added commit scripts

---
+++ jamie_commit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config --global user.name "Jamie Morton"
+git config --global user.email jamietmorton@gmail.com
+git commit -a --author="Jamie Morton <jamietmorton@gmail.com>"
+git push
